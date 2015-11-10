77e6375f4c0f3979e31b713f6c6e734349b2ba90
umagic <worzardark@gmail.com>
2015-11-10 16:13:12 +0000

    Create example.js

---
+++ example.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+var go = require('requery');
+
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: 'password',
+    database: 'database'
+});
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+go.requery('SELECT * FROM `table` WHERE `field` = 1 LIMIT 1', function (error, results) {
+  console.log('ok');
+}
+
+//I'm not sure that it works lol :D
