701f76b5d4822025f5fc470cc2e68a656b6ec875
Vladislav <worzardark@gmail.com>
2015-12-15 12:45:33 +0000

    Update requery.js

--- requery.js
+++ requery.js
@@ -2,7 +2,7 @@ function requery(query, callback) {
     connection.query(query, function (error, results, fields) {
         if (error) {
             setTimeout(function () {
-                requery(query);
+                requery(query, callback);
             }, 1000);
         } else {
             callback(results);
